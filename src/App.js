5a0af184c81422d144517a5538d1a9fbe719624f
Sergey-Sega <batman_superman@list.ru>
2022-03-31 23:04:04 +0400

    Сверстал окно авторизации

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AdminPanel } from './components/AdminPanel/AdminPanel';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <BrowserRouter>
+      <AdminPanel/>
+      </BrowserRouter>
     </div>
   );
 }
