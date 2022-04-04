b013d499f6f121d244c3ae04b3183f59984a5974
Sergey-Sega <batman_superman@list.ru>
2022-04-04 18:14:01 +0400

    Исправлены ошибки по замечаниям ментора

--- src/components/AdminPanel/Auth/Auth.jsx
+++ src/components/AdminPanel/Auth/Auth.jsx
@@ -5,7 +5,10 @@ import './style.scss';
 export const Auth = () => {
   return (
     <div className='authorization'>
-      <span className='authorization__heading'><span className='logo'></span> <h2 className='authorization__heading__head'>Need for drive</h2></span>
+      <span className='authorization__heading'>
+        <span className='logo'></span>
+         <h2 className='authorization__heading__head'>Need for drive</h2>
+         </span>
       <div className='authorization__login-block'>
         <h3 className='authorization__login-block__name'>Вход</h3>
         <form className='authorization__login-block__form'>
@@ -23,12 +26,14 @@ export const Auth = () => {
               type='password'
             />
           </fieldset>
-          <span className='authorization__login-block__form__buttons-block'><a className='link' href='#'>
+          <span className='authorization__login-block__form__buttons-block'>
+            <a className='link' href='#'>
             Запросить доступ
           </a>
           <button className='admin-button blue' type='submit'>
             Войти
-          </button></span>
+          </button>
+          </span>
         </form>
       </div>
     </div>
