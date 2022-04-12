0830d67e942d787b379b62dc1ebb7c534ea9b58d
Sergey-Sega <batman_superman@list.ru>
2022-04-12 11:34:33 +0400

    Пофиксил проблемы с деплоем

--- src/store/store.js
+++ src/store/store.js
@@ -10,8 +10,9 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, composeEnhancers(
       applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
+      window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : (f) => f,
  ));
 
 export default store;
