f9247c41446274af126dce6bf913a37a474572ce
Sergey-Sega <batman_superman@list.ru>
2022-04-14 17:52:50 +0400

    Исправление ошибок по замечаниям ментора

--- src/pages/AdminTable/SideBar/SideBar.jsx
+++ src/pages/AdminTable/SideBar/SideBar.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import './style.scss';
 import {SidebarLink} from './SideBarLink';
+import { menuSideBar } from '../../../utils/constants';
 export const Sidebar = () => {
-  const menu = [
-    { name: 'Карточка автомобиля', url: '/car-edit-card', icon: 'edit' },
-    { name: 'Список авто', url: '', icon: 'carList' },
-    { name: 'Заказы', url: '/adminPanel/orders', icon: 'orders' },
-    { name: 'Тарифы', url: '', icon: 'overview' },
-    { name: 'Создание тарифов', url: '', icon: 'forms' },
-    { name: 'Точки выдачи', url: '', icon: 'person' },
-    { name: 'Добавление точки выдачи', url: '', icon: 'error' },
-  ];
   return (
     <>
       <a className='sidebar__head'>
         <span className='logo'></span>
         <p className='par'>Need for car</p>
       </a>
-       {menu.map((el, i) => (
+       {menuSideBar.map((el, i) => (
         <SidebarLink url={el.url} title={el.name} icon={el.icon} key={i} />
       ))}
     </>
